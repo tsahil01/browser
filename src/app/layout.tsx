807eaaf47d9d05dfa08488e353d558741c1ef20b
tsahil01 <sahiltiwaskar.21@stvincentngp.edu.in>
2024-09-10 12:58:27 +0530

    vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Provider from "./providers";
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -30,6 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Provider>{children}</Provider>
+        <Analytics/>
       </body>
     </html>
   );
